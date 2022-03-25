8478bd19d2eb79893a49b2ef6dd67bbbeb3f3e3b
kedaroo <krb1202@gmail.com>
2022-03-26 00:14:05 +0530

    refactor: images

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import SingleCard from "./components/SingleCard";
 import Modal from './components/Modal';
 
 const cardImages = [
-  { "src": "/img/helmet-1.png", matched: false },
-  { "src": "/img/potion-1.png", matched: false },
-  { "src": "/img/ring-1.png", matched: false },
-  { "src": "/img/scroll-1.png", matched: false },
-  { "src": "/img/shield-1.png", matched: false },
-  { "src": "/img/sword-1.png", matched: false }
+  { "src": "/img/helmet-1.jpg", matched: false },
+  { "src": "/img/potion-1.jpg", matched: false },
+  { "src": "/img/ring-1.jpg", matched: false },
+  { "src": "/img/scroll-1.jpg", matched: false },
+  { "src": "/img/shield-1.jpg", matched: false },
+  { "src": "/img/sword-1.jpg", matched: false }
 ];
 
 export default function App() {
